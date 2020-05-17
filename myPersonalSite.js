bb7b527122cca1664819e984e755c3e6cf53b332
ayolawal <64328867+ayolawal@users.noreply.github.com>
2020-05-17 22:26:15 +0100

    Updated site with more function and sum bug fixed.

--- myPersonalSite.js
+++ myPersonalSite.js
@@ -2,18 +2,21 @@
 function openResearchBtn(researchBtn) {
   var i;
   var x = document.getElementsByClassName("researchDivs");
+  // var z = document.getElementsByClassName("btn");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
   document.getElementById(researchBtn).style.display = "block";
+  // document.getElementsByClassName(researchBtn).style.color= "#00909e"
 }
 
-function changeBackgroundColor(researchBtn) {
-  var i;
-  var x = document.getElementsByClassName("btn");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.backgroundColor = "#00909e";
-  }
-  // document.getElementById(researchBtn).style.display = "block";
-  document.getElementById(researchBtn).style.backgroundColor = "#fff";
-}
+// function changeBtnBackgroundColor(researchBtn) {
+//   var j;
+//   var y = document.getElementsByClassName("btn");
+//   for (j = 0; j < y.length; j++) {
+//     y[j].style.backgroundColor = "#00909e";
+//   }
+//   document.getElementById(researchBtn).style.backgroundColor = "#fff";
+//   // document.getElementById(researchBtn).style.color="blue";
+//   // document.getElementById(researchBtn).style.fontWeight = "bold";
+// }
